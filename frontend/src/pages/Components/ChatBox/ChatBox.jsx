f8cdd8c4e3454e52615e52bb4f8eb2aea8764883
ChamodyaGanepola <chamodyaagra2000@gmail.com>
2025-11-29 15:32:47 +0530

    checking one chat not shown error

--- frontend/src/pages/Components/ChatBox/ChatBox.jsx
+++ frontend/src/pages/Components/ChatBox/ChatBox.jsx
@@ -6,12 +6,16 @@ const ChatBox = ({ chat, currentUser, messages, userData }) => {
   const containerRef = useRef(null);
 
   useEffect(() => {
-    if (containerRef.current) containerRef.current.scrollTop = containerRef.current.scrollHeight;
+    if (containerRef.current)
+      containerRef.current.scrollTop = containerRef.current.scrollHeight;
+    console.log(currentUser);
+    console.log(messages);
   }, [messages]);
 
-  const firstLetter = userData?.firstname ? userData.firstname.charAt(0).toUpperCase() : "?";
+  const firstLetter = userData?.firstname
+    ? userData.firstname.charAt(0).toUpperCase()
+    : "?";
 
-  
   return (
     <div className="ChatBox-container">
       {chat ? (
@@ -23,20 +27,30 @@ const ChatBox = ({ chat, currentUser, messages, userData }) => {
                   className="avatar-circle"
                   style={{
                     backgroundColor:
-                      userData?.gender === "Male" ? "#3498db" :
-                      userData?.gender === "Female" ? "#ff69b4" : "#808080",
+                      userData?.gender === "Male"
+                        ? "#3498db"
+                        : userData?.gender === "Female"
+                        ? "#ff69b4"
+                        : "#808080",
                   }}
                 >
                   {firstLetter}
                 </div>
-                <div className="name">{userData?.firstname} {userData?.lastname}</div>
+                <div className="name">
+                  {userData?.firstname} {userData?.lastname}
+                </div>
               </div>
             </div>
             <hr style={{ width: "95%", border: "0.1px solid #ececec" }} />
           </div>
           <div className="chat-body" ref={containerRef}>
             {messages.map((msg) => (
-              <div key={msg._id} className={msg.senderId === currentUser ? "message own" : "message"}>
+              <div
+                key={msg._id}
+                className={
+                  msg.senderId === currentUser._id ? "message own" : "message"
+                }
+              >
                 <span>{msg.text}</span>
                 <span>{format(msg.createdAt)}</span>
               </div>
